fdd9a832040d0efc161faedae5ff78ff02aeaa6b
lavanyakamboj <kambojlavanya076@gmail.com>
2025-10-03 21:53:20 +0530

    this is addition of file

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
- for (let i = 0; i < 25; i++) {
-      let heart = document.createElement("div");
-      heart.className = "heart";
-      heart.style.left = Math.random() * 100 + "vw";
-      heart.style.animationDuration = 3 + Math.random() * 5 + "s";
-      heart.style.background = ["#ff4d6d","#ff2e63","#ff6b81"][Math.floor(Math.random()*3)];
-      document.body.appendChild(heart);
-    }
+for (let i = 0; i < 25; i++) {
+	let heart = document.createElement("div");
+	heart.className = "heart";
+	heart.style.left = Math.random() * 100 + "vw";
+	heart.style.animationDuration = 3 + Math.random() * 5 + "s";
+	heart.style.background = ["#ff4d6d", "#ff2e63", "#ff6b81"][
+		Math.floor(Math.random() * 3)
+	];
+	document.body.appendChild(heart);
+}
 
-    function openLetter() {
-      window.location.href = "letter.html";
-    }
+function openLetter() {
+	window.location.href = "letter.html";
+}
